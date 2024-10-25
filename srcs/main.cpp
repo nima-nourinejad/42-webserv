ab231bfc6992bc191dc282659ee5ccd09a2b0c3b
Alireza Sohrabizadeh <alirezasohrabizadeh@gmail.com>
2024-10-25 13:10:19 +0300

    Update main.cpp file

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,26 +6,9 @@
 /*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:43:26 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/10/22 18:29:18 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:09:37 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include "Request.hpp"
-// #include "HttpHandler.hpp"
-
-// int	main()
-// {
-// 	std::string	rawRequest = "GET /index.html HTTP/1.1\r\nHost: localhost\r\n\r\n";
-
-// 	Request	request(rawRequest);
-
-// 	HttpHandler	handler;
-// 	std::string	response = handler.handleRequest(request);
-
-// 	std::cout << response;
-
-// 	return 0;
-// }
 
 #include "HttpHandler.hpp"
 #include "CGIHandler.hpp"
@@ -64,6 +47,7 @@ int main()
 		std::cout << "POST HTTP Response:\n" << postResponse << std::endl;
 
 		// Execute CGI handler
+		std::cout << std::endl;
 		std::string cgiResponse = cgiHandler.execute(postRequest);
 		std::cout << "CGI Response:\n" << cgiResponse << std::endl;
 	}
