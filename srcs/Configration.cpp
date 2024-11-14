276577fd72c80f10ba7a27ccb605f9cce78b9fa3
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-11-14 09:38:36 +0200

    added header

--- srcs/Configration.cpp
+++ srcs/Configration.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Configration.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 09:37:08 by nnourine          #+#    #+#             */
+/*   Updated: 2024/11/14 09:37:09 by nnourine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Configration.hpp"
 
 Configration::Configration (int port, std::string const & host, size_t maxBodySize)
