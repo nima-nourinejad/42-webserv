9dfed97010ce93c5972f8c089c1d464de52a7142
Alireza Sohrabizadeh <alirezasohrabizadeh@gmail.com>
2025-02-04 13:06:13 +0200

    Main cleanup

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 09:37:19 by nnourine          #+#    #+#             */
-/*   Updated: 2025/02/03 17:36:34 by nnourine         ###   ########.fr       */
+/*   Updated: 2025/02/04 13:05:18 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,12 @@
 
 // check the host from the request and compare it with the server_name in the config file
 
-// add server name with the ip to the /etc/hosts file and after exiting the server, remove it **** it is not possible without sudo previlages
-
 //cgi is creating valgrind error
 
-// if post request has no multipart/form-data, save the body without parsing it and choose a name for it. Nginx uses sequential numbers but maybe it is hard. Timestamp could be simpler since we have the function ready.
+// if post request has no multipart/form-data, save the body without parsing it and choose a name for it.
+// Nginx uses sequential numbers but maybe it is hard. Timestamp could be simpler since we have the function ready.
 // post (not chuncked) without content-length should retutn 411
-// if the body is too big, return 413
+// if the body is too big, return 413 (the content length does not match)
 
 #include "Server.hpp"
 
